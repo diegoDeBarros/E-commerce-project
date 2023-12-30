798a3849460ad13a9906a1b4e4778f3aa03c2815
Alisson Gabriel <alissongabrielassuncao@gmail.com>
2023-12-30 15:58:10 -0300

    Adicionando carrossel da página inicial

--- e-commerce-project/src/App.tsx
+++ e-commerce-project/src/App.tsx
@@ -1,3 +1,4 @@
+import { Carousel } from "./components/global/Carousel";
 import { Categories } from "./components/global/Categories";
 import { CategoryBarContainer } from "./components/global/Categories/styles";
 import { CategoriesMobile } from "./components/global/CategoriesMobile";
@@ -10,18 +11,20 @@ import {
   VerifyUserDevice,
   VerifyUserDeviceProvider,
 } from "./contexts/verifyUserDevice";
+import { PageContainer } from "./styles";
 
 function App() {
   return (
-    <>
+    <PageContainer>
       <OpenHamburgerProvider>
         <VerifyUserDeviceProvider>
           <Header />
           <CategoriesMobile />
+          <Categories />
+          <Carousel />
         </VerifyUserDeviceProvider>
       </OpenHamburgerProvider>
-      {/* <Categories /> */}
-    </>
+    </PageContainer>
   );
 }
 
---
+++ e-commerce-project/src/styles.ts
@@ -0,0 +1,9 @@
+import styled from "styled-components"
+
+export const PageContainer = styled.div`
+display: flex;
+height: 100vh;
+width: 100vw;
+flex-direction: column;
+overflow-x: hidden;
+`
