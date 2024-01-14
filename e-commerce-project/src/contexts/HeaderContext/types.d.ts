7f1b460c9c520e39474ba59f3b7b76e6fb627e2c
Diêgo de Barros <debarrosdiego415@gmail.com>
2024-01-13 22:44:48 -0300

    folders ajust

--- e-commerce-project/src/contexts/HeaderContext/types.d.ts
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export interface HeaderContextProps {
-  children?: React.ReactNode;
-  hamburgerOpen?: boolean;
-  toggleHamburgerOpen?: () => void;
-  loginOpen?: boolean;
-  toggleLoginOpen?: () => void;
-}
--- e-commerce-project/src/contexts/types.d.ts
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-export interface VerifyUserDeviceProps{
-children?: React.ReactNode
-deviceType?: "mobile" | "tablet" | "desktop"
-
-}
