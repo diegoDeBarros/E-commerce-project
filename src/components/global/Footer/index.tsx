b80efc4b24ba32c82b092cbb1d85bcf7dc60bdea
Diêgo de Barros <debarrosdiego415@gmail.com>
2024-01-20 02:01:38 -0300

    feat: creating the footer

--- src/components/global/Footer/index.tsx
+++ src/components/global/Footer/index.tsx
@@ -11,27 +11,90 @@ import {
   LinkLi,
   LinkA,
 } from "./styles";
+
+import DiscordIconPng from "../../../icons/SocialMediaIcons/discord.png";
+import FacebookIconPng from "../../../icons/SocialMediaIcons/facebook.png";
+import PinterestIconPng from "../../../icons/SocialMediaIcons/pinterest.png";
+import TwitterIconPng from "../../../icons/SocialMediaIcons/twitter.png";
+import WhatsappIconPng from "../../../icons/SocialMediaIcons/whatsapp.png";
 export const Footer: React.FC = () => {
   return (
     <FooterWrapper>
       <FooterLogo />
       <FooterLinksWrapper>
         <LinkColumnWrapper>
-          <LinkColumnTitle>INSTITUCIONAL</LinkColumnTitle>
           <LinkListColumn>
+            <LinkColumnTitle>INSTITUCIONAL</LinkColumnTitle>
             <LinkLi>
-              <LinkA />
+              <LinkA>Onde estamos</LinkA>
+            </LinkLi>
+            <LinkLi>
+              <LinkA>Onde estamos</LinkA>
+            </LinkLi>
+            <LinkLi>
+              <LinkA>Onde estamos</LinkA>
+            </LinkLi>
+            <LinkLi>
+              <LinkA>Onde estamos</LinkA>
+            </LinkLi>
+            <LinkLi>
+              <LinkA>Onde estamos</LinkA>
+            </LinkLi>
+          </LinkListColumn>
+          <LinkListColumn>
+            <LinkColumnTitle>INSTITUCIONAL</LinkColumnTitle>
+            <LinkLi>
+              <LinkA>Onde estamos</LinkA>
+            </LinkLi>
+            <LinkLi>
+              <LinkA>Onde estamos</LinkA>
+            </LinkLi>
+            <LinkLi>
+              <LinkA>Onde estamos</LinkA>
+            </LinkLi>
+            <LinkLi>
+              <LinkA>Onde estamos</LinkA>
+            </LinkLi>
+            <LinkLi>
+              <LinkA>Onde estamos</LinkA>
+            </LinkLi>
+          </LinkListColumn>
+          <LinkListColumn>
+            <LinkColumnTitle>INSTITUCIONAL</LinkColumnTitle>
+            <LinkLi>
+              <LinkA>Onde estamos</LinkA>
+            </LinkLi>
+            <LinkLi>
+              <LinkA>Onde estamos</LinkA>
+            </LinkLi>
+          </LinkListColumn>
+          <LinkListColumn>
+            <LinkColumnTitle>INSTITUCIONAL</LinkColumnTitle>
+            <LinkLi>
+              <LinkA>Onde estamos</LinkA>
+            </LinkLi>
+            <LinkLi>
+              <LinkA>Onde estamos</LinkA>
+            </LinkLi>
+            <LinkLi>
+              <LinkA>Onde estamos</LinkA>
+            </LinkLi>
+            <LinkLi>
+              <LinkA>Onde estamos</LinkA>
+            </LinkLi>
+            <LinkLi>
+              <LinkA>Onde estamos</LinkA>
             </LinkLi>
           </LinkListColumn>
         </LinkColumnWrapper>
       </FooterLinksWrapper>
       <FooterSocialMediaWrapper>
-        <SocialMediaImg />
-        <SocialMediaImg />
-        <SocialMediaImg />
-        <SocialMediaImg />
-        <SocialMediaImg />
-        <SocialMediaImg />
+        <SocialMediaImg src={DiscordIconPng} />
+        <SocialMediaImg src={FacebookIconPng} />
+        <SocialMediaImg src={PinterestIconPng} />
+        <SocialMediaImg src={TwitterIconPng} />
+        <SocialMediaImg src={WhatsappIconPng} />
+        {/* <SocialMediaImg src={DiscordIconPng} /> */}
       </FooterSocialMediaWrapper>
     </FooterWrapper>
   );
--- src/components/global/Footer/styles.ts
+++ src/components/global/Footer/styles.ts
@@ -2,41 +2,52 @@ import styled from 'styled-components';
 
 export const FooterWrapper = styled.footer`
 
-display: grid;
-grid-template-columns: 20vw 60vw 20vw;
+display: flex;
+grid-column: span 12;
 `
 
 export const FooterLogo = styled.img`
 
-
+width: 20%;
 `
-export const FooterLinksWrapper = styled.div`
-display: grid;
-grid-template-columns: 25% 25% 25% 25%;
 
+export const FooterLinksWrapper = styled.div`
+
+width: 60%;
 `
 
 export const FooterSocialMediaWrapper = styled.div`
-
+width: 20%;
+display: flex;
+gap:4px;
 `
 
 export const SocialMediaImg = styled.img`
 
+width:20px;
+height:20px;
 `
 
 export const LinkColumnWrapper = styled.div`
 
 display: flex;
-flex-direction: column;
+flex-direction: row;
+gap: 26px;
+justify-content: center;
 `
 
-export const LinkColumnTitle = styled.h3`
-
+export const LinkColumnTitle = styled.h4`
+font-size:12px;
+font-weight: bold;
 `
 
 export const LinkListColumn = styled.ul`
 
 list-style: none;
+display:flex;
+flex-direction: column;
+gap:16px;
+font-family: dosis;
 `
 
 export const LinkLi = styled.li`
@@ -45,4 +56,5 @@ export const LinkLi = styled.li`
 
 export const LinkA = styled.a`
 
+font-size: 12px;
 `
